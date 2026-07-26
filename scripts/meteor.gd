3fc718897af71a6571065fc3b07ec201580505e0
Aaron King <86845258+kingAJK30@users.noreply.github.com>
2026-07-25 22:24:22 -0700

    changed things

--- scripts/meteor.gd
+++ scripts/meteor.gd
@@ -10,10 +10,13 @@ var player_rocket: Node2D
 
 func _ready() -> void:
 	add_to_group("space_objects")
+	add_to_group("meteors")
+	
 	origin = global_position
 	player_rocket = get_tree().get_first_node_in_group("rocket")
 	
-	body_entered.connect(_on_body_entered)
+	body_entered.connect(_on_hit)
+	area_entered.connect(_on_hit)
 
 func _process(delta: float) -> void:
 	rotation += delta * 0.5
@@ -27,6 +30,19 @@ func _process(delta: float) -> void:
 	if player_rocket and global_position.y > player_rocket.global_position.y + 1000.0:
 		queue_free()
 
-func _on_body_entered(body: Node2D) -> void:
-	if body.has_method("take_damage"):
-		body.take_damage()
+func _on_hit(node: Node2D) -> void:
+	var target_rocket: Rocket = _find_rocket(node)
+	if target_rocket:
+		target_rocket.take_damage()
+		queue_free()
+
+func _find_rocket(node: Node2D) -> Rocket:
+	if node is Rocket:
+		return node
+	if node is RocketPart and node.target_rocket:
+		return node.target_rocket
+	if node.get_parent() is RocketPart and node.get_parent().target_rocket:
+		return node.get_parent().target_rocket
+	if node.get_parent() is Rocket:
+		return node.get_parent() as Rocket
+	return null
